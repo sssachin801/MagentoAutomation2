3ef949822b888ae93baaf52cba20461a4aa2493c
sssachin801 <sssachin801@gmail.com>
2018-08-30 13:13:01 +0530

    first

---
+++ src/test/java/MagentoTest.java
@@ -0,0 +1,40 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MagentoTest {
+	@Test
+	public void magentoTest() throws Exception {
+
+		String url = "http://www.magento.com";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+
+		Home h = new Home(driver);
+		h.clickOnMyacc();
+		Thread.sleep(2000);
+
+		Login l = new Login(driver);
+		l.typeEmail("subramanyaraj87@gmail.com");
+		Thread.sleep(2000);
+		l.typePassword("Welcome123");
+		Thread.sleep(2000);
+		l.clickOnLogin();
+		Thread.sleep(2000);
+
+		Main m = new Main(driver);
+		m.clickOnLogout();
+
+		Thread.sleep(2000);
+
+		driver.quit();
+
+	}
+
+}
